4816f1c6d29be5b83987682187b36c4a7869cffe
kathirraja <kathirraja48@gmail.com>
2018-06-09 21:29:15 +0530

    changes in palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,19 @@
+
+
+
+def palindrome(n):
+    rev = 0
+    while(n>0):
+        rem = n%10
+        rev = rev*10+rem
+        n = n/10
+    return n
+
 n=int(input("enter a no:"))
-a=n
-rev=0
-while(n>0):
-    rem=n%10
-    rev=rev*10+rem
-    n=n/10
+a=palindrome(n)
 
 #print("the reverse no:",rev)
-if(a==rev):
+if(a==n):
     print("the no is palindrime")
 else:
     print("the no is not palindrome")
