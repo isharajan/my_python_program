0fe7bc1a69532a8882327f59bd6afa4bc3708a6f
kathirraja <kathirraja48@gmail.com>
2018-06-09 21:44:03 +0530

    bug fixes in palindrome file

--- palindrome.py
+++ palindrome.py
@@ -1,19 +1,19 @@
 
 
 
-def palindrome(n):
+def is_palindrome(n):
     rev = 0
+    temp = n
     while(n>0):
         rem = n%10
-        rev = rev*10+rem
+        rev = (rev*10)+rem
         n = n/10
-    return n
+    return rev==temp
 
 n=int(input("enter a no:"))
-a=palindrome(n)
-
 #print("the reverse no:",rev)
-if(a==n):
+
+if(is_palindrome(n)):
     print("the no is palindrime")
 else:
     print("the no is not palindrome")
